90e43026da5c88bd00ce805a0531563b46d43956
vs <vshaveyko@gmail.com>
2016-09-02 02:30:12 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ id_pattern = /[^\/]+/
 
 RailsApiDoc::Engine.routes.draw do
 
-  RailsApiDoc::Plugins.plugins.each do |p|
-    mount p::Engine => p.engine_path
-  end
-
   root to: 'index#index'
 
 end
