e6fec39688f14a13dfa3ac95dc9313ee9f67f081
vs <vshaveyko@gmail.com>
2016-12-14 13:45:21 +0200

    resource_params now accepts optional arguments indicating permit target

--- lib/rails_api_doc/controller/strong_params/dsl.rb
+++ lib/rails_api_doc/controller/strong_params/dsl.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+# author: Vadim Shaveiko <@vshaveyko>
 # :nodoc:
 module RailsApiDoc
   module Controller
@@ -6,9 +8,11 @@ module RailsApiDoc
 
         include RailsApiDoc::Controller::ResourceParams::PermittedParams # implements params_to_permit
 
-        def resource_params
+        def resource_params(pars = params)
+          #
           # accepted_params for permit
-          params.permit(params_to_permit)
+          #
+          pars.permit(params_to_permit)
         end
 
       end
