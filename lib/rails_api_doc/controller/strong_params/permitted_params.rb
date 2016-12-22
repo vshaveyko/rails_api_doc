eb4ba0175ebb775b31c2ca27ba282900b9ea4281
vs <vshaveyko@gmail.com>
2016-12-22 18:41:47 +0200

    controller_param may be nil -> no nesting than in strng_params

--- lib/rails_api_doc/controller/strong_params/permitted_params.rb
+++ lib/rails_api_doc/controller/strong_params/permitted_params.rb
@@ -64,7 +64,10 @@ module RailsApiDoc
             # value present but not valid for this type => skip
             next unless RailsApiDoc::Config::Validator.valid_param?(controller_param, api_param_data)
 
-            if api_param_data.ary_object? # controller_param value should be array of objects
+            if controller_param.nil?
+              accepted_params.unshift(param_name)
+
+            elsif api_param_data.ary_object? # controller_param value should be array of objects
               controller_param.each do |single_controller_param|
                 _next_nesting_level(single_controller_param,
                                     param_data: api_param_data.nested,
