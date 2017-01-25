705780ed07c17d01010783ff72d1493b50a97e71
vs <vshaveyko@gmail.com>
2017-01-25 13:06:16 +0200

    Remove redundant

--- lib/rails_api_doc/params/main.rb
+++ lib/rails_api_doc/params/main.rb
@@ -3,17 +3,6 @@
 # :nodoc:
 class RailsApiDoc::Params
 
-  module ParamStorage
-    def param_storage
-      RailsApiDoc::Controller::Request::Repository[self]
-    end
-
-    private
-
-    def responding_ctrl; end
-  end
-
-  extend ParamStorage
   include RailsApiDoc::Controller::ResourceParams::DSL
   extend RailsApiDoc::Controller::Request::DSL
 
